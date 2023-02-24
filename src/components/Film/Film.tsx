314e516a58a5193aab6d365d94bb097729020cd0
Michael Salo <michael.salo1995@gmail.com>
2023-02-25 01:35:34 +0200

    vlad changes

--- src/components/Film/Film.tsx
+++ src/components/Film/Film.tsx
@@ -1,33 +1,49 @@
-import React, {FC} from 'react';
-import {Rating} from "@mui/material";
-import {useNavigate} from "react-router-dom";
+import React, { FC } from 'react';
+import { Rating } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 import css from './Film.module.css'
-import {IMoviesRes} from "../../interfaces";
+import { IMoviesRes } from "../../interfaces";
 
 interface IProps {
-    key: number;
+    // key: number;
     film: IMoviesRes;
 
 }
 
 
-const Film:FC<IProps> = ({film}) => {
+const Film: FC<IProps> = ({ film }) => {
 
-    const {title, poster_path,vote_average, id} = film
+    const { title, poster_path, vote_average, id } = film
 
     const navigate = useNavigate();
 
     return (
-        <div className={css.filmBox} onClick={()=>navigate(id.toString())}>
-            <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title}/>
+        // <div className={css.filmBox} onClick={() => navigate(id.toString())}  >
+        //     <div className={css.imageContainer}>
+        //         <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
+        //     </div>
 
-            <div className={css.infoBox}>
-                <h4>{title}</h4>
-                <Rating name="read-only" value={vote_average} max={10} precision={0.5} readOnly />
+
+        //     <div className={css.infoBox}>
+        //         <h4>{title}</h4>
+        //         <Rating name="read-only" value={vote_average} max={10} precision={0.5} readOnly className='rating' />
+        //     </div>
+        // </div>
+        <div className={css.flipCard} onClick={() => navigate(id.toString())}>
+            <div className={css.flipCardInner}>
+                <div className={css.flipCardFront}>
+                    <img src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt={title} />
+                </div>
+                <div className={css.flipCardBack}>
+                    <div className={css.infoBox}>
+                        <h4>{title}</h4>
+                        <Rating name="read-only" value={vote_average} max={10} precision={0.5} readOnly className='rating' />
+                    </div>
+                </div>
             </div>
         </div>
     );
 };
 
-export {Film};
+export { Film };
